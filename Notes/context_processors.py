bea07ca7977717de35df53ea315b96a7099f182e
ShravanCool <shravan.akku@gmail.com>
2020-09-22 12:21:18 +0530

    Added context_processors.py

---
+++ Notes/context_processors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .forms import SearchBarform
+
+def SearchBar(request):
+    """
+    Produces a context variable for the searchbar that's available across
+    all pages.
+    """
+    return {'searchbar': SearchBarForm()}
+
+def SetCurrentCourses(request):
+    """
+    Produces a context variable for all user's current courses that's 
+    available across all pages.
+    """
+    user = request.user
+
+    try:
+        current_term = user.terms.all().filter(user=user, current=True)[0]
+        current_courses = current_term.courses.all()
+    except (AttributeError, IndexError):
+        current_courses = None
+
+    return {'current_courses': current_courses}
