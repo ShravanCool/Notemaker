efd1982494d781dbb4533f22c8513b6bf4dc9a5b
ShravanCool <shravan.akku@gmail.com>
2020-09-22 11:05:40 +0530

    Registered models

--- Notes/admin.py
+++ Notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Term, Course, ClassNote
 
 # Register your models here.
+admin.site.register(Term)
+admin.site.register(Course)
+admin.site.register(ClassNote)
