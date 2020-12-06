c60d2790926a708c73ec78601b05f1a67bfc0c28
Shravan <shravan.akku@gmail.com>
2020-12-06 21:54:05 +0530

    Modified allowed hosts

--- Notemaker/settings.py
+++ Notemaker/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# ALLOWED_HOSTS = ['scribnotesapp.herokuapp.com', 'localhost',]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['demo-notemaker.herokuapp.com', 'localhost',]
 
 INSTALLED_APPS = [
     'crispy_forms',
