5c36cb15d3690b323f3cab5f8796e79187a8884c
Shravan <shravan.akku@gmail.com>
2020-12-06 20:56:48 +0530

    Heroku deploy configs

--- Notemaker/settings.py
+++ Notemaker/settings.py
@@ -12,10 +12,10 @@ STATIC_DIR = os.path.join(STATIC_ROOT, 'notes')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['scribnotesapp.herokuapp.com', 'localhost',]
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Notemaker.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
-    )
+    ) 
 }
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Notemaker.wsgi --log-file -
